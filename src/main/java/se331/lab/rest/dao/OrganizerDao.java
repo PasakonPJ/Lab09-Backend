6e9ffce6c03ebf36661d8f3989ee4b748b8e1de4
Kongpj <pasakon_paingjai@cmu.ac.th>
2021-09-09 03:35:24 +0700

    6.1

--- src/main/java/se331/lab/rest/dao/OrganizerDao.java
+++ src/main/java/se331/lab/rest/dao/OrganizerDao.java
@@ -1,2 +1,9 @@
-package se331.lab.rest.dao;public interface OrganizerDao {
+package se331.lab.rest.dao;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import se331.lab.rest.entity.Organizer;
+
+public interface OrganizerDao {
+  Page<Organizer> getOrganizer(Pageable pageRequest);
 }
